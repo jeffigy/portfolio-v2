cfa15a22a18734b7c0c9841b13476e66b5d594e2
jeffigy <ejeffydev@gmail.com>
2024-10-05 01:18:38 +0800

    refactor: Changed the dummy data from the about page

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -11,33 +11,22 @@ const About = () => {
         className="w-full max-w-screen-md space-y-5"
       >
         <p className="text-justify text-lg">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at
-          laoreet lectus, vitae eleifend tortor. Nam quis eleifend nisi. Aenean
-          malesuada laoreet volutpat. Donec pretium nisi in neque dapibus, et
-          porta nisi finibus. Duis ac lacus quam. Nullam mattis eros vitae lorem
-          accumsan sollicitudin. Mauris quis nibh vel lacus suscipit faucibus in
-          finibus ipsum. Integer placerat diam odio, vitae ultricies velit
-          malesuada a. Vivamus et maximus tellus, eu sagittis est. Duis
-          venenatis pulvinar ligula, eleifend luctus elit sagittis sed. Cras sed
-          dui felis. Aenean lobortis dolor enim, a consectetur turpis tempor
-          fringilla.
+          Hello, I’m Jeffy Evangelista, a web developer with a thirst for
+          learning and a profound fascination with technology. From a young age,
+          I've been captivated by the inner workings of software that enable us
+          to connect and innovate.
         </p>
         <p className="text-justify text-lg">
-          Ut pulvinar vel mi ut convallis. Nunc hendrerit sit amet nulla id
-          mollis. Mauris egestas velit velit, nec eleifend odio eleifend eu. Sed
-          euismod justo odio, quis dignissim nulla ullamcorper sit amet. Aenean
-          in sapien nec dui fermentum pellentesque. Etiam interdum turpis
-          consectetur, dignissim urna non, pharetra neque. Proin ac turpis nec
-          urna accumsan ornare. Suspendisse facilisis egestas augue, non laoreet
-          libero porttitor in. Aliquam a risus non libero dictum placerat.
+          Learning is a never-ending journey, and I embrace every opportunity to
+          expand my knowledge and skills. In this ever-evolving landscape, I
+          always stay up-to-date on the latest developments, whether it's new
+          programming languages or other tools for software development.
         </p>
         <p className="text-justify text-lg">
-          Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-          per inceptos himenaeos. Nam tempus, eros non vulputate tincidunt, urna
-          leo lacinia velit, eu dignissim magna nisi ut mauris. Phasellus at
-          libero nec elit vestibulum rhoncus. Duis ac rhoncus ex, ultricies
-          aliquet justo. In ac malesuada massa. Aliquam rhoncus elit nec
-          faucibus bibendum. Proin cursus rutrum neque nec mattis.
+          When I’m not engrossed in coding, you can often find me indulging in
+          my favorite pastimes. I'm an avid anime enthusiast, a casual gamer,
+          and I also prioritize regular exercise to maintain a healthy body and
+          mind.
         </p>
       </motion.div>
     </Section>
