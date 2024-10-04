365ae86cb76597f8540fb9d216eaab98d5c260f1
jeffigy <ejeffydev@gmail.com>
2024-10-04 23:35:07 +0800

    feat: extracted Project type and added different view if project is still being developed

--- src/components/ui/Project.tsx
+++ src/components/ui/Project.tsx
@@ -1,15 +1,9 @@
+import { ProjectType } from "@/types/Project";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { Github } from "lucide-react";
 import React, { useRef } from "react";
 type ProjectProps = {
-  project: {
-    title: string;
-    description: string;
-    tags: string[];
-    imageUrl: string;
-    demoLink: string;
-    githubLink: string;
-  };
+  project: ProjectType;
 };
 
 const Project: React.FC<ProjectProps> = ({ project }) => {
@@ -31,52 +25,61 @@ const Project: React.FC<ProjectProps> = ({ project }) => {
       className="group mb-3 last:mb-0 sm:mb-8"
     >
       <section className="relative max-w-[42rem] overflow-hidden rounded-lg bg-base-200 transition sm:pr-8 sm:group-even:pl-8">
-        <div className="flex h-full flex-col space-y-5 px-5 pb-7 pt-4 sm:max-w-[50%] sm:pl-10 sm:pr-2 sm:pt-10 sm:group-even:ml-[18rem]">
-          <h3 className="text-2xl font-semibold text-slate-100">
-            {project.title}
-          </h3>
-
-          <p className="text-justify leading-relaxed text-slate-200">
-            {project.description}
-          </p>
-
-          <ul className="flex flex-wrap gap-2">
-            {project.tags.map((tag, index) => (
-              <li
-                className="rounded-full bg-base-300 px-3 py-1 text-[0.7rem] uppercase tracking-wider"
-                key={index}
-              >
-                {tag}
-              </li>
-            ))}
-          </ul>
-
-          <div className="flex gap-5">
+        {project.status === "pending" ? (
+          <div className="flex h-[400px] flex-col items-center justify-center space-y-5 px-5 pb-7 pt-4 sm:pl-10 sm:pr-2 sm:pt-10 sm:group-even:ml-[18rem]">
             {" "}
-            <a
-              aria-label="github-link"
-              target="_blank"
-              href={project.githubLink}
-              className="btn btn-ghost grow"
-            >
-              <Github />
-            </a>
-            <a
-              aria-label="demo-link"
-              target="_blank"
-              href={project.demoLink}
-              className="btn btn-primary grow"
-            >
-              Demo
-            </a>
+            <p>Under contructions...</p>
           </div>
-        </div>
+        ) : (
+          <>
+            <div className="flex h-full flex-col space-y-5 px-5 pb-7 pt-4 sm:max-w-[50%] sm:pl-10 sm:pr-2 sm:pt-10 sm:group-even:ml-[18rem]">
+              <h3 className="text-2xl font-semibold text-slate-100">
+                {project.title}
+              </h3>
 
-        <img
-          src={project.imageUrl}
-          alt="Project I worked on"
-          className="absolute -right-40 top-10 hidden w-[28.25rem] rounded-lg shadow-2xl transition group-even:-left-40 group-even:right-[initial] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 sm:block"
-        />
+              <p className="text-justify leading-relaxed text-slate-200">
+                {project.description}
+              </p>
+
+              <ul className="flex flex-wrap gap-2">
+                {project.tags.map((tag, index) => (
+                  <li
+                    className="rounded-full bg-base-300 px-3 py-1 text-[0.7rem] uppercase tracking-wider"
+                    key={index}
+                  >
+                    {tag}
+                  </li>
+                ))}
+              </ul>
+
+              <div className="flex gap-5">
+                {" "}
+                <a
+                  aria-label="github-link"
+                  target="_blank"
+                  href={project.githubLink}
+                  className="btn btn-ghost grow"
+                >
+                  <Github />
+                </a>
+                <a
+                  aria-label="demo-link"
+                  target="_blank"
+                  href={project.demoLink}
+                  className="btn btn-primary grow"
+                >
+                  Demo
+                </a>
+              </div>
+            </div>
+
+            <img
+              src={project.imageUrl}
+              alt="Project I worked on"
+              className="absolute -right-40 top-10 hidden w-[28.25rem] rounded-lg shadow-2xl transition group-even:-left-40 group-even:right-[initial] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 sm:block"
+            />
+          </>
+        )}
       </section>
     </motion.div>
   );
